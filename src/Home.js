991116a1e4ffdbfb02af68683c15f9672b179551
francisflow <Iamngigikaranja1@aol.com>
2022-07-28 12:48:57 +0300

    Added homepage and navbar component templates

---
+++ src/Home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+    return ( 
+
+        <h2>This is a homepage</h2>
+     );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+
+    
+    <nav className="allbar">
+    
+    <a href="/">Home</a>
+    <br />
+    <a href= "/create">Create</a>
+
+        </nav>
+      );
+}
+ 
+export default Navbar;
