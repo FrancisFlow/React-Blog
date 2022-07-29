32d38dc8314336a75411c273b0d54692ed7d2e09
francisflow <Iamngigikaranja1@aol.com>
2022-07-29 20:22:36 +0300

    Moved the looping logic to a different component to make it reusable

---
+++ src/BlogList.js
@@ -0,0 +1,30 @@
+const BlogList = ({blogs, title}) => {
+    return ( 
+        
+        <>
+        
+        <div className="blogs-list">
+
+            <h2> {title} </h2>
+
+
+        {
+                blogs.map((blog)=> (
+
+                <div className="blog-preview"  key={blog.id}>
+                
+                <h2>{blog.title}</h2>
+
+                <h3>Written by {blog.author}</h3>
+                </div>
+
+                ))}
+        </div>
+        
+        </>
+
+
+    );
+}
+ 
+export default BlogList;
