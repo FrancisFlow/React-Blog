fcfa5545f55d427fef79e995d8525a7f57a1c4ee
francisflow <Iamngigikaranja1@aol.com>
2022-07-29 20:23:38 +0300

    Added the bloglist component with props and title to display and filter blogs

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,28 @@
 import { useState } from "react";
-
+import BlogList from "./BlogList";
 
 const Home = () => {
 
     const [blogs, setBlogs]= useState([
         {
-            title: 'The best blogs in the world', id:1, body:'This is the way that the lord has made, we will rejoice and be glad in it'
+            title: 'The best blogs in the world', id:1, body:'This is the way that the lord has made, we will rejoice and be glad in it', author: 'Goliath'
         },
 
-        {title: 'The second best blog in the world', id:2, body:'This is the second best blog in the world'},
+        {title: 'The second best blog in the world', id:2, body:'This is the second best blog in the world', author: 'David'},
 
-    {title: 'The third best blog in the world', id:3, body:'This is the third best blog in the world'},
+    {title: 'The third best blog in the world', id:3, body:'This is the third best blog in the world', author:'Kennedy'},
     ]
     );
 
     return ( 
 
         <>
-        <div className="blogs">
+        <div className="home">
 
-            {
-                blogs.map((blog)=> (
+        <BlogList  blogs={blogs}  title="All the blogs in the world"/>
 
-                <div className="things"  key={blog.id}> 
-                
-                <h2>{blog.title}</h2>
+        <BlogList blogs={blogs.filter((blogs) =>blogs.author === "David")} title="Blogs by David" />
 
-                <h3>{blog.body}</h3>
-                </div>
-
-                ))
-            }
         </div>
     
 
