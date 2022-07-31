e372d8be487bd856bd02675813d6051516f5eba3
francisflow <Iamngigikaranja1@aol.com>
2022-07-31 14:51:58 +0300

    Added error handling in case the api call fails

--- src/Home.js
+++ src/Home.js
@@ -3,37 +3,54 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [name, setName]= useState('Sandra')
-
     const [blogs, setBlogs]= useState(null);
+    const [isPending, setIsPending]= useState(true);
+    const [error, setError]= useState(null);
 
-    const handleDelete= (id) => {
-        const newBlogs= blogs.filter(blog=> blog.id !== id);
-        setBlogs(newBlogs);
-    }
 
     useEffect(() => {   
+
+
+        setTimeout(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
+            if(!res.ok)
+            {throw Error("Couldn't resolve api call to porvided url") }
+
             return res.json();
+            
         })
+
+
         .then(data=>{
             setBlogs(data)
-        });
+            setIsPending(false);
+            setError(null)
+        })
+        .catch(err => { 
+            console.log(err.message);
+            setIsPending(false);
+            setError(err.message)
+
+        })
+    }, 1000);
+
     }, []);
 
     return ( 
 
         <>
-        <div className="home">
+        <div className="home">'
+
+        {error && <div> { error } </div>}
+        
+        {isPending && <div> <h2>Loading ......</h2></div>}
 
             { blogs &&
 
-        <BlogList  blogs={blogs}  title="All the blogs in the world" handleDelete={handleDelete}/>
+        <BlogList  blogs={blogs}  title="All the blogs in the world"/>
             }
 
-
-        <button onClick={()=> {setName('Rose')}} > I love {name}</button>
 
         </div>
     
