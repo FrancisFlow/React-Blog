498d4f105cbbd9939fdd0e45eea12e9d4d6d7bc1
francisflow <Iamngigikaranja1@aol.com>
2022-08-08 13:29:06 +0300

    Created a form to allow users to create new blogs

---
+++ src/create.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const Create = () => {
+
+    return (
+        <div className="create">
+
+            <div className="container">
+
+               
+            </div>
+        </div>
+      );
+}
+ 
+export default Create;
--- src/newBlog.js
+++ src/newBlog.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 const Create = () => {
+
+
+    const [title, setTitle]= useState('');
+    const [blogBody, setBlogBody]= useState('');
+    const [author, setAuthor]= useState('');
 
 
     return ( 
@@ -6,6 +13,35 @@ const Create = () => {
 
 <div className="create">
     <h2>Create a new blog here!</h2>
+
+    <form>
+                    <label>
+                        Blog Title:
+                    </label>
+                    <input type="text" 
+                           name="title"
+                           value={title}
+                           onChange={(e)=>{setTitle(e.target.value)}}
+
+                            />
+
+                    <label>Blog Body</label>
+
+                    <textarea type="text"
+                              value={blogBody}
+                              onChange={(e)=>{setBlogBody(e.target. value)}}
+                              />
+
+                    <label>Author</label>
+                    <input type="text"
+                           value={author}
+                           onChange={(e)=>{setAuthor(e.target.value)}}
+                           
+                           />
+
+                           <button>Add blog</button>
+
+                </form>
 </div>
 
 
